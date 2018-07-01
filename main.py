15cf53672c4824fd3c85440efef8b5a60ea48dae
swirkens <mk281272@gmail.com>
2018-07-01 06:43:14 +0200

    added animation

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from tkinter import *
 from tree import Node
 from tree import BST
 
-b = BST()
+def addMessage(newMsg) :
+    for i in range(8, -1, -1) : 
+        msg = stringList[i].get()
+        stringList[i+1].set(msg)
+    stringList[0].set(newMsg)
 
 def scroll_start(event):
     canvas.scan_mark(event.x, event.y)
@@ -11,14 +15,17 @@ def scroll_move(event):
     canvas.scan_dragto(event.x, event.y, gain=1)
     moveUIback()
 
-def moveUIback():
-    insertButton.place(x=130, y=30)
-    entryField.place(x=50, y=30)
-
 def clicked() :
     value = entryField.get()
     if value != '':
         b.insert(b.root, int(value), canvas)
+
+def moveUIback():
+    insertButton.place(x=130, y=30)
+    entryField.place(x=50, y=30)
+    for i in range(0,9):
+        labelList[i].place(x=5, y=580 - i*20)
+
 
 
 canvas = Canvas(width=600, height=600, bg='white')   
@@ -26,14 +33,27 @@ canvas.pack(expand=YES, fill=BOTH)
 canvas.bind("<ButtonPress-1>", scroll_start)
 canvas.bind("<B1-Motion>", scroll_move)
 
-insertButton = Button(canvas, text ="Dodaj", command = clicked)
+stringList = []
+for i in range(10):
+    stringList.append(StringVar())
+
+labelList = []
+for i in range(10):
+    labelList.append(Label(canvas, textvariable=stringList[i]))
+
+
+b = BST(canvas, addMessage)
+
+insertButton = Button(canvas, text ="Insert", command = clicked)
 insertButton.pack()
 
 entryField = Entry(canvas, width = 10, bd = 5)
 entryField.pack()
 
-canvas.create_window(60, 30, window = entryField)
-canvas.create_window(130,30, window = insertButton)
+canvas.create_window(0,0, window = entryField)
+canvas.create_window(0,0, window = insertButton)
+for i in range(0,9):
+    canvas.create_window(0,0, window = labelList[i])
 moveUIback()
 
 mainloop()
--- tree.py
+++ tree.py
@@ -1,3 +1,12 @@
+from time import sleep
+import threading
+
+def worker(n, tree, root, canvas):
+    n.color = 'green'
+    tree.draw(root, canvas)
+    print("Done")
+    return
+
 class Node:
     def __init__(self, parent, value, left, right):
         self.parent = parent
@@ -5,12 +14,38 @@ class Node:
         self.left = left
         self.right = right
         self.center = None
+        self.color = 'green'
 
 class BST:
-    def __init__(self):
+    def __init__(self, canvas, function):
         self.root = None
         self.min = None
         self.max = None
+        self.queue = []
+        self.list = []
+        self.msgFunction = function
+        self.thread = threading.Timer(0.0, self.threadFunction, [canvas])
+        self.thread.start()
+        
+
+
+    def threadFunction(self, canvas):
+        while True:
+            if self.queue:
+                if self.queue[0][0] == 'paintNextRed':
+                    self.queue[0][1].color = 'green'
+                    self.queue[0][3].color = 'red'
+                elif self.queue[0][0] == 'paintRed':
+                    self.queue[0][1].color = 'red'
+                elif self.queue[0][0] == 'paintGreen':
+                    self.queue[0][1].color = 'green'
+                elif self.queue[0][0] == 'paintBlue':
+                    self.queue[0][1].color = 'blue'
+                if self.queue[0][2] != '':
+                        self.msgFunction(self.queue[0][2])
+                self.draw(self.root, canvas)
+                self.queue.pop(0)
+            sleep(0.4)
 
     def move(self, n, value):
         if value < n.value: self.moveLeft(n.left, value)
@@ -31,13 +66,14 @@ class BST:
                 n.center[0] += 40     
 
     def draw(self, n, canvas):
+        if n == self.root: canvas.delete('all')
         if n:
             self.draw(n.left, canvas)
             self.draw(n.right, canvas)
             if n.parent:
                 parentCenter = n.parent.center
                 canvas.create_line(n.center[0],n.center[1],parentCenter[0],parentCenter[1], width=2)
-            canvas.create_oval(n.center[0]-20,n.center[1]-20,n.center[0]+20,n.center[1]+20,fill='green', width=2)
+            canvas.create_oval(n.center[0]-20,n.center[1]-20,n.center[0]+20,n.center[1]+20,fill=n.color, width=2)
             canvas.create_text(n.center[0],n.center[1],text=n.value)
 
     def insert(self, n, value, canvas):
@@ -47,24 +83,36 @@ class BST:
             self.min = value
             self.max = value
             n.center = [300, 50]
-            self.draw(self.root, canvas)
+            self.queue.append(('paintRed', n, 'Creating root node'))
+            self.queue.append(('paintBlue', n, ''))
+            self.queue.append(('paintGreen', n, ''))
         else:
             if value < n.value and n.left is None:
+                self.queue.append(('paintRed', n, str(value) + ' < ' + str(n.value) + '? True. Going left'))
                 n.left = Node(n, value, None, None)
+                self.queue.append(('paintNextRed', n, '', n.left))
+                self.queue.append(('paintBlue', n.left, 'Found empty node. Inserting ' + str(value)))
+                self.queue.append(('paintGreen', n.left, ''))
                 parentCenter = n.center
                 n.left.center = [parentCenter[0]-40, parentCenter[1]+40]
                 if value < self.min: self.min = value
                 else: self.moveLeft(self.root, value)
-                canvas.delete('all')
-                self.draw(self.root, canvas)
             elif value >= n.value and n.right is None:
+                self.queue.append(('paintRed', n, str(value) + ' < ' + str(n.value) + '? False. Going right'))
                 n.right = Node(n, value, None, None)
+                self.queue.append(('paintNextRed', n, '', n.right)) 
+                self.queue.append(('paintBlue', n.right, 'Found empty node. Inserting ' + str(value)))
+                self.queue.append(('paintGreen', n.right, ''))
                 parentCenter = n.center
                 n.right.center = [parentCenter[0]+40, parentCenter[1]+40]
                 if value > self.max: self.max = value
                 else: self.moveRight(self.root, value)
-                canvas.delete('all')
-                self.draw(self.root, canvas)
             else:
-                if value < n.value: self.insert(n.left, value, canvas)
-                else: self.insert(n.right, value, canvas)
+                if value < n.value:
+                    self.queue.append(('paintRed', n, str(value) + ' < ' + str(n.value) + '? True. Going right'))
+                    self.queue.append(('paintNextRed', n, '', n.left))
+                    self.insert(n.left, value, canvas)
+                else:
+                    self.queue.append(('paintRed', n, str(value) + ' < ' + str(n.value) + '? False. Going right'))
+                    self.queue.append(('paintNextRed', n, '', n.right)) 
+                    self.insert(n.right, value, canvas)
